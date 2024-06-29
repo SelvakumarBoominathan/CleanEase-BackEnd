a0422b56b3dd2e03bea579ab4fa2b894967ff611
Selvakumar Boominathan <selvans2k@gmail.com>
2024-06-29 15:13:10 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,41 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import connect from "./database/connection.js";
+
+//creating server
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use(cors());
+
+//usage of morgan library
+app.use(morgan("tiny"));
+
+//prevent hackers
+app.disable("x-powered-by");
+
+const port = 8000;
+
+//GET request
+
+app.get("/", (req, res) => {
+  res.status(201).json("Home GET Request");
+});
+
+//server listener only if there is valid Mongo connection established
+
+connect()
+  .then(() => {
+    try {
+      app.listen(port, () => {
+        console.log("server listening to the port : ", port);
+      });
+    } catch (error) {
+      console.log("Cannot connect to the server");
+    }
+  })
+  .catch((error) => {
+    console.log("Invalid database connection!");
+  });
