d32630ebc99a120b5c34143a6fc01024a5b79290
Selvakumar Boominathan <selvans2k@gmail.com>
2024-06-29 20:05:31 +0530

    changes in BE

---
+++ controllers/appcontroller.js
@@ -0,0 +1,17 @@
+// POST let registerSampleData = {
+//   firstname: "Selvakumar",
+//   lastname: "B",
+//   email: "selvans2k@gmail.com",
+//   password: "1111",
+//   confirmpassword: "1111",
+// };
+
+// file to update the user inputs
+export async function register(req, res) {
+  res.json("register route");
+}
+
+// POSt let loginData = {name: 'selva22', pass: '11111'}
+export async function login(req, res) {
+  res.json("login-route");
+}
