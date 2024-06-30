634435e449323392c291f551c81396f1e4668c97
Selvakumar Boominathan <selvans2k@gmail.com>
2024-06-30 15:39:34 +0530

    changes in routes

--- controllers/appcontroller.js
+++ controllers/appcontroller.js
@@ -11,7 +11,7 @@ export async function register(req, res) {
   res.json("register route");
 }
 
-// POSt let loginData = {name: 'selva22', pass: '11111'}
+// POST let loginData = {name: 'selva22', pass: '11111'}
 export async function login(req, res) {
   res.json("login-route");
 }
--- router/route.js
+++ router/route.js
@@ -1,20 +1,20 @@
 import { Router } from "express";
+import bcrypt from "bcrypt";
 
 const router = Router();
 
 // Create or POST method
-router.route("/Registerpage").post((req, res) => res.json("register route")); // create a new user
+router.route("/register").post((req, res) => res.json("register route")); // create a new user
 router.route("/Login").post((req, res) => res.json("Login route")); // Login to the app
-router.route("/emailverification").post((req, res) => res.json("Email route"));
-router.route("/otpvalidation").post((req, res) => res.json("OTP route"));
-router.route("/setpassword").post((req, res) => res.json("set password route"));
+router.route("/authEmail").post((req, res) => res.json("Email route")); // put OTP in the obj
+// router.route("/otpvalidation").post((req, res) => res.json("OTP route"));
+// router.route("/setpassword").post((req, res) => res.json("set password route"));
 
 // Read or GET method
-router.route("/Registerpage").get((req, res) => res.json("register route"));
-router.route("/Login").get((req, res) => res.json("Login route"));
-router.route("/generateOTP").get((req, res) => res.json("Email route")); // to generate ramdon OTP
-router.route("/VerifyOTP").get((req, res) => res.json("OTP route")); // verify generated OTP
-router.route("/ResetSession").get((req, res) => res.json("set password route"));
+router.route("/user/:username").get((req, res) => res.json("user route"));
+router.route("/genOTP").get((req, res) => res.json("Email route")); // to generate randon OTP
+router.route("/authUser").get((req, res) => res.json("OTP route")); // verify generated OTP
+// router.route("/ResetSession").get((req, res) => res.json("set password route"));
 
 // Update or PUT method
 router.route("/resetPassword").put((req, res) => res.json("reset pass route")); // reset password
