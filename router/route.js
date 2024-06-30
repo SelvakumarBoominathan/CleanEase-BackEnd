352c6e884a0333770e3d344c2742a1f4dd139a04
Selvakumar Boominathan <selvans2k@gmail.com>
2024-06-30 17:10:08 +0530

    updates in appcontrollers.js

--- router/route.js
+++ router/route.js
@@ -5,14 +5,14 @@ const router = Router();
 
 // Create or POST method
 router.route("/register").post((req, res) => res.json("register route")); // create a new user
-router.route("/Login").post((req, res) => res.json("Login route")); // Login to the app
+// router.route("/Login").post((req, res) => res.json("Login route")); // Login to the app
 router.route("/authEmail").post((req, res) => res.json("Email route")); // put OTP in the obj
 // router.route("/otpvalidation").post((req, res) => res.json("OTP route"));
 // router.route("/setpassword").post((req, res) => res.json("set password route"));
 
 // Read or GET method
-router.route("/user/:username").get((req, res) => res.json("user route"));
-router.route("/genOTP").get((req, res) => res.json("Email route")); // to generate randon OTP
+router.route("/user/:username").get((req, res) => res.json("user route")); // get the user details
+router.route("/genOTP").get((req, res) => res.json("Email route")); // to generate random OTP
 router.route("/authUser").get((req, res) => res.json("OTP route")); // verify generated OTP
 // router.route("/ResetSession").get((req, res) => res.json("set password route"));
 
