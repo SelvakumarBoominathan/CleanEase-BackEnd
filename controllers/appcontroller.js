ad38d5f2b6382e48a46444cfa9f8e5ec65734fda
Selvakumar Boominathan <selvans2k@gmail.com>
2024-06-30 17:10:08 +0530

    updates in appcontrollers.js

--- controllers/appcontroller.js
+++ controllers/appcontroller.js
@@ -1,17 +1,47 @@
-// POST let registerSampleData = {
-//   firstname: "Selvakumar",
-//   lastname: "B",
-//   email: "selvans2k@gmail.com",
-//   password: "1111",
-//   confirmpassword: "1111",
-// };
+// POST request to create new user
+//   "firstname": "Selvakumar",
+//   "lastname": "B",
+//   "email": "selvans2k@gmail.com",
+//   "password": "1111",
+//   "confirmpassword": "1111",
+//    http://localhost:8000/api/register
 
 // file to update the user inputs
 export async function register(req, res) {
-  res.json("register route");
+  res.json("Register a new user");
 }
 
-// POST let loginData = {name: 'selva22', pass: '11111'}
+// POST req to login {name: 'selva22', pass: '11111'}
+// http://localhost:8000/api/login
 export async function login(req, res) {
-  res.json("login-route");
+  res.json("User LOGIN");
+}
+
+// GET req to login {name: 'selva22', pass: '11111'}
+// http://localhost:8000/api/user/:username
+export async function getUser(req, res) {
+  res.json("User Details");
+}
+
+// GET req to generate otp in user Obj
+// get user details from userdata {email: 'selva@gmail.com'}
+// update otp details to user {otp: '22222'}
+// http://localhost:8000/api/generateOTP
+export async function updateOTP(req, res) {
+  res.json("Generate OTP to user obj");
+}
+
+// GET req to verifyOTP otp in user Obj
+// get user details from userdata {email: 'selva@gmail.com'}
+// update otp details to user {otp: '22222'}
+// http://localhost:8000/api/verifyOTP
+export async function verifyOTP(req, res) {
+  res.json("verifyOTP OTP in user obj");
+}
+
+// PATCH req to update the password
+// {newpassword: '111111111s$S', reenterpassword: '111111111s$S'}
+// http://localhost:8000/api/updatepassword
+export async function updatePassword(req, res) {
+  res.json("Password has been updated");
 }
