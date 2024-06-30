fdfa442f6bf168cdd40c8d288e870c23ef8907ae
Selvakumar Boominathan <selvans2k@gmail.com>
2024-06-30 23:32:13 +0530

    changes implemented

--- controllers/appcontroller.js
+++ controllers/appcontroller.js
@@ -38,29 +38,25 @@ export async function register(req, res) {
   }
 }
 
-// POST req to login {name: 'selva22', pass: '11111'}
+// POST req to login
 // http://localhost:8000/api/login
 export async function login(req, res) {
   res.json("User LOGIN");
 }
 
-// GET req to login {username: 'selva22', password: '11111'}
+// GET req to login
 // http://localhost:8000/api/user/:username
 export async function getUser(req, res) {
   res.json("User Details");
 }
 
 // GET req to generate otp in user Obj
-// get user details from userdata {email: 'selva@gmail.com'}
-// update otp details to user {otp: '22222'}
 // http://localhost:8000/api/generateOTP
 export async function generateOTP(req, res) {
   res.json("Generate OTP to user obj");
 }
 
 // GET req to verifyOTP otp in user Obj
-// get user details from userdata {email: 'selva@gmail.com'}
-// update otp details to user {otp: '22222'}
 // http://localhost:8000/api/verifyOTP
 export async function verifyOTP(req, res) {
   res.json("verifyOTP OTP in user obj");
@@ -72,7 +68,6 @@ export async function createResetSession(req, res) {
 }
 
 // PATCH req to update the password
-// {newpassword: '111111111s$S', reenterpassword: '111111111s$S'}
 // http://localhost:8000/api/updatepassword
 export async function resetPassword(req, res) {
   res.json("Password has been updated");
