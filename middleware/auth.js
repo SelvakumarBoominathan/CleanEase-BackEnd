13a3fa749360d2882f3512bd1bd5d27e73739c4f
Selvakumar Boominathan <selvans2k@gmail.com>
2024-07-03 23:29:30 +0530

    updated middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,5 @@
-import jwt from "jsonwebtoken";
-
-export function localVariable(req, res, next) {
-  //middleware to create variable when gen otp triggered
+export function localVariables(req, res, next) {
+  //middleware to create variable when generate otp triggered
   req.app.locals = {
     OTP: null,
     resetSession: false,
