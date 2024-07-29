1431daf7a76f5470e949faba542c233c2ca8dd38
Selvakumar Boominathan <selvans2k@gmail.com>
2024-07-29 23:55:28 +0530

    resolving errors

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,8 +1,10 @@
 export function localVariables(req, res, next) {
   //middleware to create variable when generate otp triggered
-  req.app.locals = {
-    OTP: null,
-    resetSession: false,
-  };
+  if (!req.app.locals.OTP) {
+    req.app.locals = {
+      OTP: null,
+      resetSession: false,
+    };
+  }
   next();
 }
