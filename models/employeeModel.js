ade73e2f914ba1c813c4a1a817614381944d77bf
Selvakumar Boominathan <selvans2k@gmail.com>
2024-08-16 00:52:36 +0530

    changes in employee model

--- models/employeeModel.js
+++ models/employeeModel.js
@@ -3,21 +3,27 @@ import mongoose from "mongoose";
 const employeeSchema = new mongoose.Schema({
   image: {
     type: String,
+    required: [true, "Please provide image URL"],
   },
   name: {
     type: String,
+    required: [true, "Please provide name"],
   },
   category: {
     type: String,
+    required: [true, "Please provide category"],
   },
   city: {
     type: String,
+    required: [true, "Please provide city"],
   },
   id: {
     type: Number,
+    required: [true, "Please provide id"],
   },
   price: {
     type: Number,
+    required: [true, "Please provide number"],
   },
 });
 
