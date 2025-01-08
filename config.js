cd6719d77cf1ca06c696c92b0847a2a7a7d31090
Selvakumar Boominathan <selvans2k@gmail.com>
2025-01-08 17:24:04 +0530

    changes in .env file

--- config.js
+++ config.js
@@ -6,7 +6,10 @@ export default {
   name: {},
   Email: process.env.EMAIL,
   Password: process.env.PASSWORD,
-  ATLAS_URI:
-    "mongodb+srv://admin:admin123@cluster-cleanease-be-fi.qir72.mongodb.net/",
+  ATLAS_URI: process.env.URI,
+  // ATLAS_URI:
+  //   "mongodb+srv://admin:admin123@cluster-cleanease-be-fi.qir72.mongodb.net/",
 };
 // console.log("JWT_SECRET:", process.env.JWT_SECRET);
+
+//"mongodb+srv://admin:admin123@cluster-cleanease-be-fi.qir72.mongodb.net/myDatabase?retryWrites=true&w=majority"
